36541ce7f113e8d4c111d7646d530a86e86cc4b0
Matteo Bruscagnin <matteo@iMac.local>
2021-06-14 17:51:12 +0200

    aggiunto input URL articolo#

--- paywall_bypass.py
+++ paywall_bypass.py
@@ -1,18 +1,20 @@
+import os
 import urllib.request as r
-page = r.urlopen('https://nuovavenezia.gelocal.it/venezia/cronaca/2021/06/12/news/blitz-della-finanza-in-centro-a-venezia-bancarelle-nel-mirino-scattano-sequestri-di-prodotti-e-maxi-multe-1.40381982')
+page = r.urlopen(input("Inserisci URL dell'articolo de LA NUOVA VENEZIA da leggere: "))
+
 # @return prima posizione marker di inizio
 string = page.read().decode('utf8')
+#string = input("Inserisci URL dell'articolo da leggere: ")
 startPos1 = string.find("<!--  -->")
 
-# @return ultima posiione marker di inizio
+# @return ultima posizione marker di inizio
 endPos1 = string.rfind("<!--  -->")
 
 # @return prima posizione marker di fine
 startPos2 = string.find("<!--  -->", endPos1)
 
 text_file = open("paywall.html", "w")
-text_file.write(string)
+text_file.write("<html><body>"+string[startPos1:startPos2]+"</body></html>")
 text_file.close()
 
-
-# print("<html><body>"+string[startPos1:startPos2]+"</body></html>")
+os.system("open paywall.html")
