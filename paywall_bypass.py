63495d81437798c972b13c41c85ff6804a7bf655
Matteo Bruscagnin <10279526+MBruscagnin@users.noreply.github.com>
2021-08-21 11:21:25 +0000

    Update paywall_bypass.py
    Code adapted to the site of the Venice local newspaper "La Nuova di Venezia e Mestre"

--- paywall_bypass.py
+++ paywall_bypass.py
@@ -2,16 +2,17 @@ import os
 import urllib.request as r
 page = r.urlopen(input("Inserisci URL dell'articolo de LA NUOVA VENEZIA da leggere: "))
 
-# @return prima posizione marker di inizio
-string = page.read().decode('utf8')
 #string = input("Inserisci URL dell'articolo da leggere: ")
-startPos1 = string.find("<!--  -->")
+string = page.read().decode('utf8')
+
+# @return prima posizione marker di inizio
+startPos1 = string.find("<!-- ZEPHR_FEATURE ql_nuovavenezia_article -->")
 
 # @return ultima posizione marker di inizio
-endPos1 = string.rfind("<!--  -->")
+endPos1 = string.rfind("<!-- ZEPHR_FEATURE ql_nuovavenezia_article -->")
 
 # @return prima posizione marker di fine
-startPos2 = string.find("<!--  -->", endPos1)
+startPos2 = string.find("<!-- ZEPHR_FEATURE_END ql_nuovavenezia_article -->", endPos1)
 
 text_file = open("paywall.html", "w")
 text_file.write("<html><body>"+string[startPos1:startPos2]+"</body></html>")
